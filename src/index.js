4ea44a4f615e367b669ae6fffa0950b638986fea
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2022-07-20 15:19:24 +0400

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
-// module.exports = function check(str, bracketsConfig) {
- 
-// }
-
-function check(str, bracketsConfig) {
-  let isOpen = false;
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
 
   for (let i = 0; i < str.length; i++) {
     const currentBracket = str[i];
-    const currentBracketsPair = bracketsConfig.find((item) => item.includes(currentBracket));
-    const openingBracket = currentBracketsPair[0];
-    const closingBracket = currentBracketsPair[1];
-
-    console.log(`\n---------- Cycle ${i + 1} starts ----------`);
-    console.log(`Brackets status: ${isOpen ? 'open' : 'closed'}`);
-    console.log(`Current bracket at index ${i}: ${currentBracket}`);
-    console.log(`Current brackets pair:  ${currentBracketsPair.join('')}`);
-
-    if (currentBracket === openingBracket && !isOpen) {
-      console.log(`Brackets open, current bracket ${currentBracket}`);
-
-      isOpen = true;
-    } else if (currentBracket === openingBracket && isOpen) {
-      //check(str.slice[i])
-    } else if (currentBracket === closingBracket && isOpen) {
-      console.log(`Brackets closed, current bracket ${currentBracket}`);
-
-      isOpen = false;
-      // return true;
-    } else if (currentBracket === closingBracket && !isOpen) {
-      console.log(`Closing bracket, without opening one, current bracket ${currentBracket}`);
-      console.log(`Closing bracket ${closingBracket}`);
-
-      return false;
-    }
+    const currentConfig = bracketsConfig.find((item) => item.includes(currentBracket));
+    const openingBracket = currentConfig[0];
+    const closingBracket = currentConfig[1];
+   
+    if (currentBracket === closingBracket && stack[stack.length - 1] === openingBracket) {
+      stack.pop();
+    } else stack.push(currentBracket);
   }
-
-  return !isOpen;
+ 
+  return !stack.length;
 }
 
-console.log(check('[()]()', [['(', ')'], ['[', ']']]));
